dbbffabfe02331740464cedd1d9f363799eeb9cb
sameerCr7 <sameer05072001@gmail.com>
2024-05-20 12:07:38 +0530

    code update

--- app.py
+++ app.py
@@ -3,14 +3,13 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceInstructEmbeddings
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
-from langchain_community.llms import HuggingFaceHub
+
 
 import os
 load_dotenv()
